788328da585b7c207d65fe9f3613c0aa5fc1e20f
ParkeyZ <269172228@qq.com>
2019-10-12 13:30:05 +0800

    in mian.py

--- main.py
+++ main.py
@@ -7,26 +7,23 @@ from tensorflow import keras
 # Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
-from LoadFiles import load_files
 
 
 print(tf.__version__)
 
-# === Import the Fashion MNIST dataset ===
-
-train_images, train_labels, test_images, test_labels = load_files()
-
+# Import the Fashion MNIST dataset
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-
 
 # Write a class named LossHistory to save loss and acc
 class LossHistory(keras.callbacks.Callback):
     def on_train_begin(self, logs={}):
         self.losses = {'batch':[], 'epoch':[]}
         self.acc = {'batch':[], 'epoch':[]}
-        self.val_losses = {'batch': [], 'epoch': []}
-        self.val_acc = {'batch': [], 'epoch': []}
+        self.val_losses = {'batch':[], 'epoch':[]}
+        self.val_acc = {'batch':[], 'epoch':[]}
 
     def on_batch_end(self, batch, logs={}):
         self.losses['batch'].append(logs.get('loss'))
@@ -41,34 +38,35 @@ class LossHistory(keras.callbacks.Callback):
         self.val_acc['epoch'].append(logs.get('val_accuracy'))
 
     def loss_plot(self, loss_type, curve_color, curve_label):
-        iters = range(len(self.losses[loss_type]))
-        plt.figure(1)
-        plt.plot(iters, self.val_losses[loss_type], curve_color, label='loss'+curve_label)
-        plt.grid(True)
-        plt.xlabel(loss_type)
-        plt.ylabel('loss')
-        plt.legend(loc="upper right")
-        plt.figure(2)
-        plt.plot(iters, self.val_acc[loss_type], curve_color, label='acc'+curve_label)
-        plt.grid(True)
-        plt.xlabel(loss_type)
-        plt.ylabel('acc')
-        plt.legend(loc="upper right")
+            plt.figure(1)
+            iters = range(len(self.val_losses[loss_type]))
+            plt.plot(iters, self.val_losses[loss_type], curve_color, label='vloss'+curve_label)
+            plt.grid(True)
+            plt.xlabel(loss_type)
+            plt.ylabel('val_loss')
+            plt.legend(loc="upper right")
+            plt.figure(2)
+            iters = range(len(self.val_acc[loss_type]))
+            plt.plot(iters, self.val_acc[loss_type], curve_color, label='vacc'+curve_label)
+            plt.grid(True)
+            plt.xlabel(loss_type)
+            plt.ylabel('val_acc')
+            plt.legend(loc="lower right")
 
 
-def train_and_plot(model_struc, x_train, y_train, x_test, y_test, optimer, curve_color, curve_label):
+def train_and_plot(model_struc, x_train, x_test, optimer, curve_color, curve_label):
     # Compile the model
     model_struc.compile(optimizer=optimer,
                         loss='sparse_categorical_crossentropy',
                         metrics=['accuracy'])
     # Train the model
-    model_struc.fit(x_train, y_train,
+    model_struc.fit(x_train, train_labels,
               batch_size=1000, epochs=10, verbose=2,
-              validation_data=(x_test, y_test),
+              validation_data=(x_test, test_labels),
               callbacks=[history])
     # Evaluate accuracy
-    test_loss, test_acc = model_struc.evaluate(x_test, y_test, verbose=2)
-    print('\nTest accuracy:', test_acc)
+    test_loss, test_acc = model_struc.evaluate(x_test, test_labels, verbose=2)
+    print('\nTest accuracy:', test_acc, '\n\n')
     # plot loss curve
     history.loss_plot('epoch', curve_color, curve_label)
 
@@ -81,28 +79,21 @@ test_images = test_images / 255.0
 train_images_conv = train_images_conv / 255.0
 test_images_conv = test_images_conv / 255.0
 
-
 # creat a instanse of LossHistory
 history = LossHistory()
 
 # Set up the layers
 model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),
-    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Conv2D(16, kernel_size=(3,3), activation='relu', input_shape=(28, 28, 1)),
+    keras.layers.MaxPool2D(pool_size=(2,2), strides=(2,2)),
+    keras.layers.Flatten(),
+    keras.layers.Dense(1000, activation='relu'),
     keras.layers.Dense(10, activation='softmax')
 ])
 
-model_1 = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),
-    keras.layers.Dense(128, activation='relu'),
-    keras.layers.Dense(10, activation='softmax')
-])
-
-train_and_plot(model, train_images, train_labels, test_images, test_labels, 'adam', 'r', '(Adam)')
-
-train_and_plot(model_1, train_images, train_labels, test_images, test_labels, 'sgd', 'g', '(SGD)')
+train_and_plot(model, train_images_conv, test_images_conv, 'adam', 'r', '(relu)')
 
 plt.show()
 
 # save model
-# model.save('mod1.h5')
+model.save('mod1.h5')
